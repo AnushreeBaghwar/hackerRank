0bb8b9a2c9b28706a0effbab64b577e933fa7b02
Nikky Amresh <nikkyamresh8@gmail.com>
2020-09-25 16:01:42 +0530

    changed files

--- script.py
+++ script.py
@@ -9,11 +9,12 @@ for file in listOfFiles:
 	title = ''
 	for line in lines:
 		if "https://www.hackerrank.com/" in line:
-			link = line.split()[1]
+			qlink = line.split()[1]
 			# print(link)
 			readme = open("readme.md","a")
-			title = link.split('/')[4].replace('-',' ').capitalize()
-			text = '- [{0}]({1}) \n\n'.format(title,link)
+			title = qlink.split('/')[4].replace('-',' ').capitalize()
+			link = 'https://github.com/emoanu/hackerRank/blob/master/'+title.replace(' ','%20')+'.py'
+			text = '\n{2}\n - [Question]({0})\n - [Solution]({1})\n\n'.format(qlink,link,title)
 			readme.write(text)
 	f.close()
 	os.rename(file,title+".py")
